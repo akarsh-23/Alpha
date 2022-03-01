cc0310c35f65d2b749cb54b375ba9b8acf314750
Akarsh Shrivastava <akarsh_shrivastava@yahoo.com>
2022-03-01 20:02:46 +0530

    commit 1.0.1

--- projects/alpha/src/app/components/home/home.component.ts
+++ projects/alpha/src/app/components/home/home.component.ts
@@ -10,7 +10,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.image = '/assets/img/dev.jpg';
+    this.image = '/assets/img/Profile.png';
   }
 
 }
